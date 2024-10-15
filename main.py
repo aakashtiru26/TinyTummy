fc63db326503530e0d3f9a2f003df5c32d9044cb
Tiruveedula Aakash <aakashtiru26@gmail.com>
2024-10-15 21:10:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ from PIL import Image
 
 # Initialize Firebase Admin SDK
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase-adminsdk.json")
+    firebase_creds = {
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    }
+    cred = credentials.Certificate(firebase_creds)
     firebase_admin.initialize_app(cred, {
-        'storageBucket': 'tinytummy-13311.appspot.com'  # Use the correct bucket name without gs://
+        'storageBucket': 'tinytummy-13311.appspot.com'  # Correct bucket name
     })
 
 # Access the storage bucket
